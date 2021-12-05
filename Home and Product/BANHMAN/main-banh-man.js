b12a0241d3555dd45d21ec4e4d384f49238395e9
trantantai1004 <91689329+trantantai1004@users.noreply.github.com>
2021-12-05 21:13:14 +0700

    Add files via upload

---
+++ Home and Product/BANHMAN/main-banh-man.js
@@ -0,0 +1,16 @@
+
+function banhSungBo (){
+    window.location.href = "banh-man-1.html"
+}
+function banhMuffin (){
+    window.location.href = "banh-man-2.html"
+}
+function banhBaguette (){
+    window.location.href = "banh-man-3.html"
+}
+function banhChaBong() {
+    window.location.href = "banh-man-4.html"
+}
+function banhAmann() {
+    window.location.href = "banh-man.html"
+}
